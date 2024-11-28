e720109907d6f5cc2ff9178da2743cdb7931b534
Wagner Santos <35262291-wagnsantos02@users.noreply.replit.com>
2024-11-28 01:46:59 +0000

    fix: foi corrigido o bug de validação, try e catch foram adicionados ao controllers

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import authRoutes from "./routes/authRoutes";
+import bookRoutes from "./routes/bookRoutes";
 
 export const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/auth", authRoutes); // Prefixo para as rotas de autenticação
+app.use("/", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
